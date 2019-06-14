4b3fdf7e090220353024586f78a8a10bb65de560
Marcus Norrgard <marcus.norrgard@walkbase.com>
2019-06-14 15:12:43 +0300

    u

--- pan_tilt_tracking.py
+++ pan_tilt_tracking.py
@@ -68,80 +68,62 @@ def set_servos(pan, tlt):
     signal.signal(signal.SIGINT, signal_handler)
     # loop indefinitely
     while True:
-		# the pan and tilt angles are reversed
-		panAngle = -1 * pan.value
-		tiltAngle = -1 * tlt.value
-
-		# if the pan angle is within the range, pan
-		if in_range(panAngle, servoRange[0], servoRange[1]):
-			pth.pan(panAngle)
-
-		# if the tilt angle is within the range, tilt
-		if in_range(tiltAngle, servoRange[0], servoRange[1]):
-			pth.tilt(tiltAngle)
-
+        # the pan and tilt angles are reversed
+        panAngle = -1 * pan.value
+        tiltAngle = -1 * tlt.value
+        # if the pan angle is within the range, pan
+        if in_range(panAngle, servoRange[0], servoRange[1]):
+            pth.pan(panAngle)
+        # if the tilt angle is within the range, tilt
+        if in_range(tiltAngle, servoRange[0], servoRange[1]):
+            pth.tilt(tiltAngle)
 # check to see if this is the main body of execution
 if __name__ == "__main__":
-	# construct the argument parser and parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-c", "--cascade", type=str, required=True,
-		help="path to input Haar cascade for face detection")
-	args = vars(ap.parse_args())
-
-    	# start a manager for managing process-safe variables
-	with Manager() as manager:
-		# enable the servos
-		pth.servo_enable(1, True)
-		pth.servo_enable(2, True)
-
-		# set integer values for the object center (x, y)-coordinates
-		centerX = manager.Value("i", 0)
-		centerY = manager.Value("i", 0)
-
-		# set integer values for the object's (x, y)-coordinates
-		objX = manager.Value("i", 0)
-		objY = manager.Value("i", 0)
-
-		# pan and tilt values will be managed by independed PIDs
-		pan = manager.Value("i", 0)
-		tlt = manager.Value("i", 0)
-
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--cascade", type=str, required=True, help="path to input Haar cascade for face detection")
+    args = vars(ap.parse_args())
+    with Manager() as manager:
+        # enable the servos
+        pth.servo_enable(1, True)
+        pth.servo_enable(2, True)
+        # set integer values for the object center (x, y)-coordinates
+        centerX = manager.Value("i", 0)
+        centerY = manager.Value("i", 0)
+        # set integer values for the object's (x, y)-coordinates
+        objX = manager.Value("i", 0)
+        objY = manager.Value("i", 0)
+        # pan and tilt values will be managed by independed PIDs
+        pan = manager.Value("i", 0)
+        tlt = manager.Value("i", 0)
         # set PID values for panning
-		panP = manager.Value("f", 0.09)
-		panI = manager.Value("f", 0.08)
-		panD = manager.Value("f", 0.002)
-
-		# set PID values for tilting
-		tiltP = manager.Value("f", 0.11)
-		tiltI = manager.Value("f", 0.10)
-		tiltD = manager.Value("f", 0.002)
-
+        panP = manager.Value("f", 0.09)
+        panI = manager.Value("f", 0.08)
+        panD = manager.Value("f", 0.002)
+        # set PID values for tilting
+        tiltP = manager.Value("f", 0.11)
+        tiltI = manager.Value("f", 0.10)
+        tiltD = manager.Value("f", 0.002)
         # we have 4 independent processes
-		# 1. objectCenter  - finds/localizes the object
-		# 2. panning       - PID control loop determines panning angle
-		# 3. tilting       - PID control loop determines tilting angle
-		# 4. setServos     - drives the servos to proper angles based
-		#                    on PID feedback to keep object in center
-		processObjectCenter = Process(target=obj_center,
-			args=(args, objX, objY, centerX, centerY))
-		processPanning = Process(target=pid_process,
-			args=(pan, panP, panI, panD, objX, centerX))
-		processTilting = Process(target=pid_process,
-			args=(tlt, tiltP, tiltI, tiltD, objY, centerY))
-		processSetServos = Process(target=set_servos, args=(pan, tlt))
-
-		# start all 4 processes
-		processObjectCenter.start()
-		processPanning.start()
-		processTilting.start()
-		processSetServos.start()
-
-		# join all 4 processes
-		processObjectCenter.join()
-		processPanning.join()
-		processTilting.join()
-		processSetServos.join()
-
-		# disable the servos
-		pth.servo_enable(1, False)
-		pth.servo_enable(2, False)
+        # 1. objectCenter  - finds/localizes the object
+        # 2. panning       - PID control loop determines panning angle
+        # 3. tilting       - PID control loop determines tilting angle
+        # 4. setServos     - drives the servos to proper angles based
+        #                    on PID feedback to keep object in center
+        processObjectCenter = Process(target=obj_center,args=(args, objX, objY, centerX, centerY))
+        processPanning = Process(target=pid_process,args=(pan, panP, panI, panD, objX, centerX))
+        processTilting = Process(target=pid_process,args=(tlt, tiltP, tiltI, tiltD, objY, centerY))
+        processSetServos = Process(target=set_servos, args=(pan, tlt))
+        # start all 4 processes
+        processObjectCenter.start()
+        processPanning.start()
+        processTilting.start()
+        processSetServos.start()
+        # join all 4 processes
+        processObjectCenter.join()
+        processPanning.join()
+        processTilting.join()
+        processSetServos.join()
+        # disable the servos
+        pth.servo_enable(1, False)
+        pth.servo_enable(2, False)
